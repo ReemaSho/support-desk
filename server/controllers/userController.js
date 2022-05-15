77b079c7fdf35398417f968be27f0f0b8508e3a3
Reema <Reemaalshohof88@gmail.com>
2022-05-16 01:13:59 +0200

    add controllers and routes for user

---
+++ server/controllers/userController.js
@@ -0,0 +1,7 @@
+const registerUser = (req, res) => {
+    res.send("Register user");
+};
+const loginUser = (req, res) => {
+    res.send("Login user");
+};
+export { registerUser, loginUser };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Support Desk API" });
+});
+app.use("/api/users", userRoutes);
 app.listen(PORT, console.log(`Server started at port ${PORT}`));
