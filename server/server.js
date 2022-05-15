93167abf0ae71b606c6cf963fa41437ebb58dd54
Reema <Reemaalshohof88@gmail.com>
2022-05-16 00:28:13 +0200

    run the server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,6 @@
-console.log("server...");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+const app = express();
+app.listen(PORT, console.log(`Server started at port ${PORT}`));
