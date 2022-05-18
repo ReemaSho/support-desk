5e16f70004474426c0d79adbde26a4b421ca68e6
Reema <Reemaalshohof88@gmail.com>
2022-05-18 11:12:40 +0200

    build users schema

---
+++ server/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+}, { timeStamps: true });
+
+export default mongoose.model("User", userSchema);
