84006c05c98621f9ace03de2d52063fa3ef1c510
Reema <Reemaalshohof88@gmail.com>
2022-05-18 11:59:46 +0200

    built register user controller, hash passwords and save users in databases

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,48 @@
 import asyncHandler from "express-async-handler";
-
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
 //@desc    Register a new user
 //@route   /api/user
 //@access  Public
 const registerUser = asyncHandler(async(req, res) => {
     const { name, email, password } = req.body;
+
     if (!name || !email || !password) {
         res.status(400);
         throw new Error("Please include all fields");
     }
-    res.send("Register user");
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+    if (user) {
+        const newUser = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        };
+
+        res.status(201).json({
+            success: true,
+            user: {...newUser },
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
 });
 
 //@desc    Login a user
