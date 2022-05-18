504777607ce39cf8ff6f01db7170c4440c979043
Reema <Reemaalshohof88@gmail.com>
2022-05-18 10:49:36 +0200

    connect database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import colors from "colors";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+//connect to database
+connectDB();
+
+// set up the app
 const app = express();
 
 app.use(express.json());
@@ -16,5 +22,9 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", userRoutes);
+
+// middleware
 app.use(errorHandler);
+
+//
 app.listen(PORT, console.log(`Server started at port ${PORT}`));
