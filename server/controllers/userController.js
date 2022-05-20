6ec2a7a3a0d6a9564435785f8d421f36abad8c2a
Reema <Reemaalshohof88@gmail.com>
2022-05-20 10:07:27 +0200

    add protected route for authinticate users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 //@desc    Register a new user
-//@route   /api/user
+//@route   /api/users
 //@access  Public
 const registerUser = asyncHandler(async(req, res) => {
     const { name, email, password } = req.body;
@@ -48,7 +48,7 @@ const registerUser = asyncHandler(async(req, res) => {
 });
 
 //@desc    Login a user
-//@route   /api/user/login
+//@route   /api/users/login
 //@access  Public
 const loginUser = asyncHandler(async(req, res) => {
     const { email, password } = req.body;
@@ -75,8 +75,20 @@ const loginUser = asyncHandler(async(req, res) => {
     }
 });
 
+//@desc Get current user
+//@route   /api/users/me
+//@access  Private
+const getMe = asyncHandler(async(req, res) => {
+    const { _id: id, email, name } = req.user;
+    const user = {
+        id,
+        email,
+        name,
+    };
+    res.status(200).send(user);
+});
 // jWT token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
-export { registerUser, loginUser };
+export { registerUser, loginUser, getMe };
