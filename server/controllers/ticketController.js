a30c0121c399297539ae921a71041a34eb8af191
Reema <Reemaalshohof88@gmail.com>
2022-07-07 23:23:39 +0200

    add get single ticket delete and update controllers

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -3,7 +3,7 @@ import Ticket from "../models/ticketModel.js";
 import User from "../models/userModel.js";
 
 //@desc   get user tickets
-//@route   Get /api/tickets
+//@route   GET /api/tickets
 //@access  Private
 const getTickets = asyncHandler(async(req, res) => {
     const { id: userId } = req.user;
@@ -17,8 +17,37 @@ const getTickets = asyncHandler(async(req, res) => {
     res.status(200).send(tickets);
 });
 
+//@desc   get user ticket
+//@route   GET /api/tickets/:id
+//@access  Private
+const getTicket = asyncHandler(async(req, res) => {
+    const { id: userId } = req.user;
+    const { id: ticketId } = req.params;
+    // check user
+    const user = await User.findById(userId);
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    // find ticket
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found");
+    }
+
+    // check if ticket belongs to the user from JWT
+    if (ticket.user.toString() !== userId) {
+        res.status(401);
+        throw new Error("Not authorized");
+    }
+
+    res.status(200).send(ticket);
+});
+
 //@desc   create ticket
-//@route   Post /api/tickets
+//@route   POST /api/tickets
 //@access  Private
 const createTicket = asyncHandler(async(req, res) => {
     const { product, description } = req.body;
@@ -44,4 +73,63 @@ const createTicket = asyncHandler(async(req, res) => {
     res.status(201).send(ticket);
 });
 
-export { getTickets, createTicket };
+//@desc   delete user ticket
+//@route   DELETE /api/tickets/:id
+//@access  Private
+const deleteTicket = asyncHandler(async(req, res) => {
+    const { id: userId } = req.user;
+    const { id: ticketId } = req.params;
+    // check user
+    const user = await User.findById(userId);
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    // find ticket
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found");
+    }
+
+    // check if ticket belongs to the user from JWT
+    if (ticket.user.toString() !== userId) {
+        res.status(401);
+        throw new Error("Not authorized");
+    }
+    await ticket.remove();
+
+    res.status(200).send({ success: true });
+});
+
+//@desc   update user ticket
+//@route   PUT /api/tickets/:id
+//@access  Private
+const updateTicket = asyncHandler(async(req, res) => {
+    const { id: userId } = req.user;
+    const { id: ticketId } = req.params;
+    // check user
+    const user = await User.findById(userId);
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    // find ticket
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found");
+    }
+
+    //check if ticket belongs to the user from JWT
+    if (ticket.user.toString() !== userId) {
+        res.status(401);
+        throw new Error("Not authorized");
+    }
+    const updatedTicket = await Ticket.findByIdAndUpdate(ticketId, req.body);
+
+    res.status(200).send(updatedTicket);
+});
+export { getTickets, getTicket, createTicket, deleteTicket, updateTicket };
--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { getTickets, createTicket } from "../controllers/ticketController.js";
+import {
+    getTickets,
+    getTicket,
+    createTicket,
+    deleteTicket,
+    updateTicket,
+} from "../controllers/ticketController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+    .route("/:id")
+    .get(protect, getTicket)
+    .delete(protect, deleteTicket)
+    .put(protect, updateTicket);
 
 export default router;
