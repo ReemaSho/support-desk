da08753520b0b418b0a928d82c84eb30b33aae43
Reema <Reemaalshohof88@gmail.com>
2022-07-09 23:48:15 +0200

    FE create a new tickect functionality

--- client/src/features/tickets/ticketSlice.js
+++ client/src/features/tickets/ticketSlice.js
@@ -10,6 +10,26 @@ const initialState = {
     message: "",
 };
 
+// create new ticket
+export const createTicket = createAsyncThunk(
+    "tickets/create",
+    async(ticketData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().authentication.user.token;
+            return await ticketService.createTicket(ticketData, token);
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
 export const ticketSlice = createSlice({
     name: "ticket",
     initialState,
@@ -23,8 +43,21 @@ export const ticketSlice = createSlice({
             state.message = "";
         },
     },
-    extraReducers: (builder) => {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(createTicket.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(createTicket.fulfilled, (state) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+            })
+            .addCase(createTicket.rejected, (state, action) => {
+                state.isLoading = false;
+                state.message = action.payload;
+            });
+    },
 });
 
-export const { state } = ticketSlice.actions;
+export const { reset } = ticketSlice.actions;
 export default ticketSlice.reducer;
--- client/src/pages/NewTicket.jsx
+++ client/src/pages/NewTicket.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
+import Spinner from "../components/Spinner";
 
 const NewTicket = () => {
   const { user } = useSelector((state) => state.authentication);
+  const { isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.ticket
+  );
   const [name] = useState(user.name);
   const [email] = useState(user.email);
   const [product, setProduct] = useState("iPhone");
   const [description, setDescription] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess) {
+      dispatch(reset());
+      navigate("/tickets");
+    }
+    dispatch(reset());
+  }, [isError, isLoading, isSuccess, message, dispatch, navigate]);
+
   const onSubmit = (e) => {
     e.preventDefault();
+    dispatch(createTicket({ product, description }));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <>
       <section className="heading">
